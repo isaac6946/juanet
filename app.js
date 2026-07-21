c9c4eb0251457978e32ef883c6626269c95cd8fc
isaac6946 <isaacnjunge69@gmail.com>
2026-07-21 12:42:41 +0300

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-Backendless.initApp('16FFD132-607E-43A7-8EBE-10A4FC1D0A95', '17F7CF5F-EC5A-41A9-854E-16C03097888A');
-
-Backendless.Data.of('TestTable').save({ message: 'Hello from my app' })
-  .then(result => console.log('Saved:', result))
-  .catch(err => console.error('Error:', err));
